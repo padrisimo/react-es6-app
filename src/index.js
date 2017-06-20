90b5cf154fe3b6ae9f4373b4560a6ff0e5b6bd12
padrisimo <nopuedoparardemolar@outlook.com>
2017-06-20 15:59:46 +0200

    manage state

--- src/index.js
+++ src/index.js
@@ -2,5 +2,5 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-ReactDOM.render(<App cat={5} txt="Probando el generador create-react-app" />,
+ReactDOM.render(<App />,
   document.getElementById('root'));
