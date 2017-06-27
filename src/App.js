638caa2c7b3f9dc8de34b574b6231bb7b4675b45
padrisimo <nopuedoparardemolar@outlook.com>
2017-06-27 16:58:57 +0200

    Understand React.Children Utilities

--- src/App.js
+++ src/App.js
@@ -1,43 +1,22 @@
-import React from 'react';
-import './App.css';
+import React, {Component} from 'react';
 
-class App extends React.Component {
-  constructor(){
-    super();
-    this.state = {
-      input: '/* add your jsx here */',
-      output: '',
-      err: ''
-    }
-  }
-  update(e){
-    let code = e.target.value;
-    try {
-      this.setState({
-        output: window.Babel
-        .transform(code, { presets: ['es2015', 'react']})
-        .code, 
-        err: ''
-      })
-    }
-    catch(err){
-      this.setState({err: err.message})
-    }
-  }
+class App extends Component{
   render(){
     return(
-      <div>
-        <header>{this.state.err}</header>
-        <div className="container">
-          <textarea 
-          onChange={this.update.bind(this)}
-          defaultValue={this.state.input}/>
-          <pre>
-            {this.state.output}
-          </pre>
-        </div>
-      </div>
+      <Parent>
+        <div className="childA"></div>
+        <div className="childB"></div>
+      </Parent>
     )
+  }
+}
+
+class Parent extends Component {
+  render(){
+    let items = React.Children
+      .map(this.props.children, child => child) 
+    console.log(items)
+    return null
   }
 }
 
